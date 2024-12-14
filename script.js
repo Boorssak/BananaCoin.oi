ac6f249b5ab98a4ff31a1db0fd82232740e0d063
Boorssak <104225010+Boorssak@users.noreply.github.com>
2024-12-14 23:51:42 +0100

    Add files via upload

---
+++ script.js
@@ -0,0 +1,6 @@
+document.addEventListener('DOMContentLoaded', function() {
+    const btn = document.querySelector('.btn');
+    btn.addEventListener('click', function() {
+        alert('Welcome to the BananaCoin community!');
+    });
+});
