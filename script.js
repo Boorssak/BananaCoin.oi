679574ebd40fc051d1f9d1b61b9da9502def433a
Boorssak <104225010+Boorssak@users.noreply.github.com>
2024-12-14 23:48:40 +0100

    Delete script.js

--- script.js
+++
@@ -1,6 +0,0 @@
-document.addEventListener('DOMContentLoaded', function() {
-    const btn = document.querySelector('.btn');
-    btn.addEventListener('click', function() {
-        alert('Welcome to the BananaCoin community!');
-    });
-});
